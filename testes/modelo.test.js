0e3b2823bcfe32fcb74eaeb0e6ddb82631c482fe
RodolfoLessa <rodolfolessa.eng@gmail.com>
2022-11-26 23:51:07 -0300

    Incluindo alterações

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -16,10 +16,18 @@ test('Testando cadastro de três perguntas', () => {
   modelo.cadastrar_pergunta('1 + 1 = ?');
   modelo.cadastrar_pergunta('2 + 2 = ?');
   modelo.cadastrar_pergunta('3 + 3 = ?');
+  modelo.cadastrar_resposta(0,'2');
+  modelo.get_respostas(0);
+  modelo.get_pergunta(0);
+
   const perguntas = modelo.listar_perguntas(); 
+  
   expect(perguntas.length).toBe(3);
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+  
+
+
 });
